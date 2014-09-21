79a1db490659ee6d3b540879195a730ca1a86f36
Nick Boen <NickABoen@gmail.com>
2014-09-21 14:04:41 -0600

    Updating using statements in main project. Also successfully tested in a windows 8 environment.

--- Manic Shooter/Manic Shooter/Game1.cs
+++ Manic Shooter/Manic Shooter/Game1.cs
@@ -1,7 +1,14 @@
-﻿using Microsoft.Xna.Framework;
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
 using TextPackage;
+#endregion
 
 namespace Manic_Shooter
 {
